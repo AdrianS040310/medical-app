d406758e58243130458fb70f37a8e57c8a061fcb
Adrian <jose13699631@gmail.com>
2025-09-27 20:26:37 -0600

    Refactor tab layout by removing the explore screen and adding appointments, map, and profile screens. Updated tab icons and styles for improved UI consistency.

---
+++ app/(tabs)/appointments.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AppointmentsScreen() {
+  return (
+    <View>
+      <Text>Gestiona tus citas médicas</Text>
+    </View>
+  );
+}
