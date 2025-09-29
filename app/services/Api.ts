93ee4b84d639e0712d8fd61779dd5ba303fe766a
Adrian <jose13699631@gmail.com>
2025-09-29 00:40:41 -0600

    Remove MapScreen component and refactor API key usage in getHealthNews function for improved clarity and consistency.

--- app/services/Api.ts
+++ app/services/Api.ts
@@ -1,12 +1,12 @@
-// services/newsApi.ts
-import { NEWS_API_KEY } from "@env"; // importa tu API Key desde .env
+import { NEWS_API_KEY } from '@env';
+
+const apiKeyNews = NEWS_API_KEY;
 
 export async function getHealthNews() {
   try {
-    const url = `https://newsapi.org/v2/everything?q=health%20OR%20medicine&language=es&apiKey=${NEWS_API_KEY}`;
+    const url = `https://newsapi.org/v2/everything?q=health%20OR%20medicine&language=es&apiKey=${apiKeyNews}`;
     const response = await fetch(url);
 
-    //  Verifica si la API responde con un error HTTP
     if (!response.ok) {
       return {
         success: false,
@@ -24,14 +24,14 @@ export async function getHealthNews() {
     } else {
       return {
         success: false,
-        error: "No se encontraron noticias en este momento.",
+        error: 'No se encontraron noticias en este momento.',
       };
     }
   } catch (error) {
     // 👀 Error de conexión, sin internet o timeout
     return {
       success: false,
-      error: "No hay conexión a internet o la API no respondió.",
+      error: 'No hay conexión a internet o la API no respondió.',
     };
   }
 }
